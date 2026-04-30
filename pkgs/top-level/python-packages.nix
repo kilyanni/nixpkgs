3eaaf5ccba16281dc8bef9dc1218b07c0e2dfe59
kilyanni <github@nkilian.org>
2026-04-30 20:25:21 +0200

    python3Packages.bittensor-cli: init at 9.21.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2114,6 +2114,8 @@ self: super: with self; {
 
   bitstruct = callPackage ../development/python-modules/bitstruct { };
 
+  bittensor-cli = callPackage ../development/python-modules/bittensor-cli { };
+
   bittensor-drand = callPackage ../development/python-modules/bittensor-drand { };
 
   bittensor-wallet = callPackage ../development/python-modules/bittensor-wallet { };
